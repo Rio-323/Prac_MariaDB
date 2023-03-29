b98a3e12e6d973e497d86a8bc8b5fb23f77948e8
Rio-323 <kimsiyeon5214@gmail.com>
2023-03-29 11:52:31 +0900

    3.29

--- ex08.sql
+++ ex08.sql
@@ -16,3 +16,10 @@ select distinct deptno, job from emp order by deptno;
 
 -- 부서, 직책별 평균, 합계,,,
 select deptno, job, sum(sal) from emp group by deptno, job;
+
+-- 합계가 2000 이상.
+-- select deptno, job, sum(sal) from emp where sum(sal) >= 2000 group by deptno, job; -> 에러 발생 where 다음에는 group 함수를 사용할 수 없음.
+select deptno, job, sum(sal) from emp group by deptno, job having sum(sal) >= 2000;
+
+--부서별 인원수 6명 이상인 부서번호, 인원 수 출력
+select deptno, count(ename) from emp group by deptno having count(ename) >= 6;
