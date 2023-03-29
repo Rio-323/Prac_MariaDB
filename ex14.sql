4f87a2ce328c4bc143ee1be3e05d9ba9b24335e5
Rio-323 <kimsiyeon5214@gmail.com>
2023-03-29 14:53:11 +0900

    3.29

---
+++ ex14.sql
@@ -0,0 +1,7 @@
+-- select * from emp cross join dept; -> 무작위 결합 -> 무결성 오류
+-- select * from emp inner join dept; -> 무작위 결합 -> 무결성 오류 -> 조건을 주어야 함.
+select * from emp inner join dept where emp.deptno = dept.deptno order by dept.deptno;
+
+select * from emp inner join dept where emp.deptno = dept.deptno and emp.deptno = 10;
+
+select * from emp e inner join dept d where e.deptno = d.deptno and e.deptno = 10;
