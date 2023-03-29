e061f56cd8b76501a357b7830f92544e49cf3548
Rio-323 <kimsiyeon5214@gmail.com>
2023-03-29 11:39:59 +0900

    3.29

--- ex08.sql
+++ ex08.sql
@@ -6,3 +6,13 @@ select deptno, count(*) from emp group by deptno;
 SELECT deptno, ename, max(sal)
 FROM emp
 group by deptno;
+
+-- 직책별 평균급여
+SELECT job, truncate(avg(sal), 0)
+FROM emp
+group by job;
+
+select distinct deptno, job from emp order by deptno;
+
+-- 부서, 직책별 평균, 합계,,,
+select deptno, job, sum(sal) from emp group by deptno, job;
