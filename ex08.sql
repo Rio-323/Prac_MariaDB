5483a14a410f93c8ccf51b45f375dcf5865c331b
Rio-323 <kimsiyeon5214@gmail.com>
2023-03-29 11:30:38 +0900

    3.29

--- ex08.sql
+++ ex08.sql
@@ -1,3 +1,8 @@
 -- 부서별 데이터 개수
 select count(*) from emp group by deptno;
 select deptno, count(*) from emp group by deptno;
+
+-- 부서별 최대 급여
+SELECT deptno, ename, max(sal)
+FROM emp
+group by deptno;
