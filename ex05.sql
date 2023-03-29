9a66a1ff8df846aae648310f328f0d33a975890e
Rio-323 <kimsiyeon5214@gmail.com>
2023-03-29 10:38:56 +0900

    3.29

--- ex05.sql
+++ ex05.sql
@@ -2,9 +2,8 @@ select empno, ename, deptno,
 case deptno 
     when 10 then '회계' 
     when 20 then '조사' 
-    when 30 then '영업' 
-    when 40 then '총무' 
-    else '' 
+    when 30 then '영업'  
+    else '총무' 
 end '부서명' 
 from emp
 order by deptno;
