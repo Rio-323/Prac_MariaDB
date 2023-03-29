cdc5143107e79793ae02229c09e1aeaf25e9cd1e
Rio-323 <kimsiyeon5214@gmail.com>
2023-03-29 10:00:18 +0900

    3.29

---
+++ ex01.sql
@@ -0,0 +1,1 @@
+select sal * 12 + ifnull(comm, 0) from emp where deptno = 30;
---
+++ ex02.sql
@@ -0,0 +1,1 @@
+select empno, ename, sal, comm, truncate(sal * 12 + ifnull(comm, 0), 0) '연봉' from emp where deptno = 30;
