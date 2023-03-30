81852c3898aa002b018dd8ab3eec3409a61d988a
Rio-323 <kimsiyeon5214@gmail.com>
2023-03-30 12:37:55 +0900

    3.30

--- 3.30/ex03.sql
+++ 3.30/ex03.sql
@@ -15,4 +15,7 @@ alter table emp_alter modify job varchar(5); -> 데이터가 들어있으면 오
 
 alter table emp_alter rename column job to work; -> 컬럼명 바꾸기
 
-alter table emp_alter drop work; -> 컬럼 삭제
+alter table emp_alter drop work; -> 컬럼 삭제(데이터가 있어도 삭제)
+
+alter table emp_alter2 rename emp_alter3; -> 테이블 이름 변경
+rename table emp_alter3 to emp_alter2; -> 테이블 명을 바꾸는 다른 방법
