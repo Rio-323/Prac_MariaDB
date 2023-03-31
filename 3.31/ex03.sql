28d0369a7bd55bc8c27eb9b15fbd397dcb2c5263
Rio-323 <kimsiyeon5214@gmail.com>
2023-03-31 12:12:24 +0900

    3.31

---
+++ 3.31/ex03.sql
@@ -0,0 +1,36 @@
+create table products (
+    pno varchar(5) primary key,
+    pname varchar(20),
+    cost int(8),
+    stock int(5)
+);
+
+create table orders (
+    orderno varchar(10) primary key,
+    orderdate varchar(8),
+    address varchar(50),
+    phone varchar(20),
+    status varchar(5),
+    cno varchar(5),
+    constraint foreign key(cno) references customers(cno)
+);
+
+create table customers (
+    cno varchar(5) primary key,
+    cname varchar(10),
+    address varchar(50),
+    email varchar(20),
+    phone varchar(20)
+);
+
+create table orderdetail (
+    orderno varchar(10) primary key,
+    pno varchar(10) primary key,
+    qty int(5),
+    cost int(8),
+    constraint foreign key(pno) references products(pno),
+    constraint foreign key(orderno) references orders(orderno)
+);
+
+
+
---
+++ 3.31/ex04.sql
@@ -0,0 +1,30 @@
+create table products (
+    pno varchar(5),
+    pname varchar(20),
+    cost int(8),
+    stock int(5)
+);
+
+create table orders (
+    orderno varchar(10),
+    orderdate varchar(8),
+    address varchar(50),
+    phone varchar(20),
+    status varchar(5),
+    cno varchar(5)
+);
+
+create table customers (
+    cno varchar(5),
+    cname varchar(10),
+    address varchar(50),
+    email varchar(20),
+    phone varchar(20)
+);
+
+create table orderdetail (
+    orderno varchar(10),
+    pno varchar(10),
+    qty int(5),
+    cost int(8)
+);
